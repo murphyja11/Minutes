cb57a4bf304ecbe9599c25bac0706abe4530329c
Jack <jack_murphy@brown.edu>
2020-08-15 14:33:47 -0400

    some progress

---
+++ Minutes/Views/Signup View/EscapeChev.swift
@@ -0,0 +1,21 @@
+//
+//  EscapeChev.swift
+//  Minutes
+//
+//  Created by Jack Murphy on 8/15/20.
+//  Copyright © 2020 Jack Murphy. All rights reserved.
+//
+
+import SwiftUI
+
+struct EscapeChev: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EscapeChev_Previews: PreviewProvider {
+    static var previews: some View {
+        EscapeChev()
+    }
+}
