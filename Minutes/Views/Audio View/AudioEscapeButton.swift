d7b4e3d998aac91b830367d637a6cd6767c732c9
Jack <jack_murphy@brown.edu>
2020-08-16 18:39:57 -0400

    have a bug when trying to play the audio

---
+++ Minutes/Views/Audio View/AudioEscapeButton.swift
@@ -0,0 +1,21 @@
+//
+//  AudioEscapeButton.swift
+//  Minutes
+//
+//  Created by Jack Murphy on 8/16/20.
+//  Copyright © 2020 Jack Murphy. All rights reserved.
+//
+
+import SwiftUI
+
+struct AudioEscapeButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AudioEscapeButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AudioEscapeButton()
+    }
+}
